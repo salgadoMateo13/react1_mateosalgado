6fb2dc511337a15582296fc92847df0fa0e7ab50
msalgado13 <msalgado13cuc.edu.co>
2024-09-18 09:57:02 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import miImagen from "./assets/img/logoReact.png";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="Logo react" />
+        <h1>Hola Mundo!!!</h1>
+        <p>Este es el primer proyecto en react...</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
